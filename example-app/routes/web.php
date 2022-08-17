45ce680408adf6c727133d68897084f47041bd10
Viviana <vivitr5195@gmail.com>
2022-08-17 19:32:19 +0200

    create files

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/animals', function()
+{
+    return view('animals');
+});
+
+//Route::get('/animals', [AnimalsController::class,'index']);
